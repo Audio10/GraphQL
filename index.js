ca26f6affc138ad3404d5858b3f1e3ca9307412b
Claudio Jesus Vazquez Villanueva <claudiojesus_10@hotmail.com>
2019-11-30 21:33:00 -0600

    2_Queries_y_Resolvers

--- index.js
+++ index.js
@@ -5,12 +5,23 @@ const { graphql, buildSchema } = require('graphql')
 // definiendo el esquema
 const schema = buildSchema(`
     type Query{
-        hello: String
+        hello: String,
+        saludo: String
     }
 `)
 
+// Configurar los resolvers
+const resolvers = {
+    hello: ()=> {
+        return 'Hola Mundo'
+    },
+    saludo: ()=>{
+        return 'Hola a todos'
+    }
+}
+
 // Ejecutar el query hello
-graphql(schema, '{hello }').then( (data)=> {
+graphql(schema, '{ saludo }', resolvers).then( (data)=> {
     console.log(data)
 })
 
